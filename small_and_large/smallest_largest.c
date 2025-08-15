fa208b908a8240377a8c91846c7f788f783c2012
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-08-15 13:39:17 +0200

    .

--- small_and_large/smallest_largest.c
+++ small_and_large/smallest_largest.c
@@ -1,3 +1,7 @@
+// Author: N Aimable
+// Purpose: The program recieves four integer and displays largest and smallest
+// Date: 15/08/2025
+
 #include <stdio.h>
 
 int main(void)
@@ -6,20 +10,29 @@ int main(void)
     printf("Enter four integers: ");
     scanf("%d%d%d%d", &n1, &n2, &n3, &n4);
 
-    // What would be the logic (that's no the best approach but it works.)
+    // Below would be the logic
+    // comparing them in groups of two
+    //  - and using a ternary ops to determine the ultimate largest or smallest
+    // NOTE: that's most probably 'NOT' the best approach but it works :)
 
-    if (n1 > n2 && n3 > n4) {
+    if (n1 > n2 && n3 > n4)
+    {
         printf("Largest: %d\n", (n1 > n3) ? n1 : n3);
         printf("Smallest: %d\n", (n2 < n4) ? n2 : n4);
-    } else if (n2 > n1 && n3 > n4) {
+    }
+    else if (n2 > n1 && n3 > n4)
+    {
         printf("Largest: %d\n", (n2 > n3) ? n2 : n3);
         printf("Smallest: %d\n", (n1 < n4) ? n1 : n4);
-    } else if (n1 > n2 && n4 > n3) {
+    }
+    else if (n1 > n2 && n4 > n3)
+    {
         printf("Largest: %d\n", (n1 > n4) ? n1 : n4);
         printf("Smallest: %d\n", (n2 < n3) ? n2 : n3);
-    } else if (n2 > n1 && n4 > n3) {
+    }
+    else if (n2 > n1 && n4 > n3)
+    {
         printf("Largest: %d\n", (n2 > n4) ? n2 : n4);
         printf("Smallest: %d\n", (n1 < n3) ? n1 : n3);
     }
-
 }
