0ec2c15c75972b5637caf11a95d0b983eb44e56b
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-08-15 12:51:17 +0200

    .

--- time/time.c
+++ time/time.c
@@ -5,6 +5,9 @@
 
 int main(void)
 {
+    // This program assumes that the enter format will be a valid 24hour format.
+    // i.e it doesn't handle invalid format (&& it may return unexpected info)
+
     int hour = 0;
     int min = 0;
     printf("Enter a 24-hour time: ");
@@ -18,14 +21,26 @@ int main(void)
         printf("Equavelent 12-hour time: %.2d:%.2d ", hour, min);
     }
 
-    // adding the PM or AM on display.
-    switch (hour) {
-        case 12: case 13: case 14:
-        case 15: case 16: case 17:
-        case 18: case 19: case 20:
-        case 21: case 22: case 23: printf("PM\n"); break;
-        default: printf("AM\n");
-    }
+    // The other simplest
+    printf("%s\n", hour >= 12 ? "PM" : "AM");
+
+    // adding the PM or AM on display. (simplified if version)
+    // if (hour >= 12)
+    //     printf("PM\n");
+    // else
+    //     printf("AM\n");
+
+    // The most simplified (using ternary)
+    // (hour >= 12 && hour < 24) ? printf("PM\n") : printf("AM\n");
+
+    // adding the PM or AM on display using switch (this could be the longer version).
+    // switch (hour) {
+    //     case 12: case 13: case 14:
+    //     case 15: case 16: case 17:
+    //     case 18: case 19: case 20:
+    //     case 21: case 22: case 23: printf("PM\n"); break;
+    //     default: printf("AM\n");
+    // }
 
     return 0;
 }
---
+++ wind_force_scale/wind.c
@@ -0,0 +1,26 @@
+// Author: N Aimable
+// Purpose: Displays wind force description given user input in knots
+// Date: 15/08/2025
+
+#include <stdio.h>
+
+int main(void)
+{
+    int wind_speed = 1;
+
+    printf("Enter Wind Speed (in knots): ");
+    scanf("%d", &wind_speed);
+
+    if (wind_speed > 63)
+        printf("Hurricane\n");
+    else if (wind_speed > 47)
+        printf("Storm\n");
+    else if (wind_speed > 27)
+        printf("Gale\n");
+    else if (wind_speed > 3)
+        printf("Breeze\n");
+    else if (wind_speed > 0)
+        printf("Light air\n");
+    else
+        printf("Calm\n");
+}
