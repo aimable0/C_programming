4d65e3b780a8385e569f804b305a0eb014225e46
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-08-19 22:54:44 +0200

    c

--- loops_exercises/exercises.c
+++ loops_exercises/exercises.c
@@ -43,24 +43,24 @@ int main(void)
 
     // exercise 5
     // exercise 6
-    // Task: translating program fragment exe1 into a single for statement
+    // Task: Translating program fragment exe1 into a single for statement
     // exercise 1
     // int i = 1;
     printf("------\n");
     i = 1;
     while (i <= 128) {
-        printf("%d\n", i);
+        printf("%d ", i);
         i *= 2;
     }
-    printf("------\n");
+    printf("\n--------\n");
 
     // its equivalent single for statement
-    for(int i = 1; i <= 128; i *= 2) printf("%d\n", i);
-    printf("------\n");
+    for(int i = 1; i <= 128; i *= 2) printf("%d ", i);
+    printf("\n-------\n");
 
 
     // exercise 7
-    // task: translate the program fragment of exe2 int a single for statement
+    // Task: translate the program fragment of exe2 int a single for statement
 
     // exe2 while:
     i = 9384;
@@ -90,13 +90,12 @@ int main(void)
     // printf("\n--------\n");
 
     // exercise 10
-    // task: Show how to replace a continue statement by an equivalent goto statement.
+    // Task: Show how to replace a continue statement by an equivalent goto statement.
     // example with a continue
     // program: let's write a short example that print even and skips (continues) when odd number reached
     for (int i = 0; i < 10; i++) {
         if (i % 2 != 0)
             continue; // since it's a single statement no need for braces.
-
         printf("%d ", i); // Expected: 0 2 4 6 8;
     }
     printf("\n--------\n");
@@ -106,20 +105,45 @@ int main(void)
         // it's worth noting that 'int i = 0;' create a variable i that's available only in this current for.
         if (i% 2 != 0)
             goto end;
-
         printf("%d ", i);
         end:;   // label name is 'end' the ';' following end: indicates that it's a null statement.
     }
     printf("\n--------\n");
 
     // exercise 11
-    // What output does the following program fragment produce?
-    
+    // Task: What output does the following program fragment produce?
+    int sum = 0;
+    for (i = 0; i < 10; i++) {
+        if (i % 2)    // Expected: to be true when i % 2 results into something other than 0 ex 2 % 2 results into 1
+            continue; // skipping odd ( since odd number would result i number other than zero (0) which makes the if true)
+        sum += i;   // Expected: 2 + 4 + 6 + 8
+    }
+    printf("%d\n", sum);
 
 
     // exercise 12
+
+    printf("\n--------\n");
     // exercise 13
+    // Task: Rewrite the following loop so that its body is empty:
+    int n, m;
+    for (n = 0; m > 0; n++)
+        m /= 2;
+
+    // how it can be written with an empty body.
+    for (n = 0; m > 0; n++, m /= 2);
+
+    // a more preferrable clear way to write it would be
+    for (n = 0; m > 0; n++, m /= 2)
+        /* empty body */;
+
+
     // exercise 14
+    // Task: Find the error in the following program fragment and fix it.
+    if (n % 2 == 0); // the ';' on this line terminates the if and considers it to be an if with null statement body
+        printf("n is even\n");
+        // Error: printf("n is even\n"); should be printf("%d is even\n", n);
+
 
     printf("\n-----------\n");
     return 0;
