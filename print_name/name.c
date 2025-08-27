509a6ad6c8ea68d4f9dd6bac3f09af78da48e146
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-08-27 06:14:51 +0200

    .

--- print_name/name.c
+++ print_name/name.c
@@ -1,6 +1,46 @@
 #include <stdio.h>
+#include <ctype.h>
 
 int main(void)
 {
-    
+    printf("Enter a first and last name: ");
+    char ch;
+    short i = 0;
+    char first_char;
+    // we want to use carriage return and exit..
+    short second_name_len = 0;
+    short first_name_len = 0;
+
+    while((ch = getchar())) {
+        // let's store the first letter.
+        if (i == 0) {
+            first_char = ch;
+        }
+        i++;
+
+        if (ch == ' ') {
+            first_name_len = i;
+            printf("\r");
+        }
+        else {
+            if (ch == '\n') {
+                printf(", %c.", first_char);
+
+                // placebo fix ..
+                second_name_len = i - first_name_len;
+
+
+                //!\ next fix .. how do we deal with extra spaces..
+                
+                for (int m = 0; m < (first_name_len - second_name_len) + 2; m++) {
+                    printf(" ");
+                }
+
+                break; //end loop.
+            } else {
+                printf("%c", ch);
+            }
+        }
+    }
+    printf("\n");
 }
