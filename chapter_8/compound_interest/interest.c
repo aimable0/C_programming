a03c07e58adb03e1b9a3a0a4b461bd6b914951c8
aimable0 <aimablenkurikiyimana1@gmail.com>
2025-09-01 19:12:11 +0200

    .

---
+++ chapter_8/compound_interest/interest.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#define CAPITAL 100
+
+int main(void)
+{
+
+    int rate, years;
+    int capital = CAPITAL;
+    printf("Enter interest: ");
+    scanf("%d", &rate);
+    printf("Enter a number of years: ");
+    scanf("%d", &years);
+
+    // Let's do it for the constant rate
+
+    int interest = capital * rate;
+    interest =
+    printf("Years\t6%%\n");
+    printf("  %d  \n", years--);
+
+}
