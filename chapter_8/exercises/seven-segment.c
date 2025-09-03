d0aac626551582b92d7cc3c62f3662ee15385497
aimable0 <aimablenkurikiyimana1@gmail.com>
2025-09-03 15:20:51 +0200

    .

---
+++ chapter_8/exercises/seven-segment.c
@@ -0,0 +1,9 @@
+/* */
+#include <stdio.h>
+
+int main(void)
+{
+    // using the seven-segment display to display 0 - 10 digits.
+    // const int segments[10][7] = {{1, 1, 1, 1, 1, 1, 0}, {0, 1, 1}, {1, 1, 0, 1, 1, 0, 1}, {...}}
+    return 0;
+}
